d045d5737dfddd267447e97ea5d637943bde7dcb
EC2 Default User <ec2-user@ip-172-31-40-129.ec2.internal>
2019-05-15 08:45:00 +0000

    07 commit

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,5 +12,18 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         //
+        DB::table('posts')->indert([
+            [
+                'title' =>  '最初の記事',
+                'body'  => '最初の記事のテキストです。',
+                'created_at' => '2017-05-02 14:28:19',
+                'updated_at' => '2017-05-02 14:28:19'
+            ],[
+                'title' => '2番目の記事',
+                'body' => '2番目の記事のテキストです。',
+                'created_at' => '2017-05-03 14:28:19',
+                'updated_at' => '2017-05-03 14:28:19'
+            ]
+        ]);
     }
 }
